6aa92f1059c57dd68229b6706fe5ac6fcb762c16
DDR23 <roboreserva.andre@gmail.com>
2024-08-21 21:56:16 -0300

    feat:{adicionando leitor de json e README.md}

---
+++ README.md
@@ -0,0 +1,3 @@
+# atividade---backend---meu-primeiro-socket---others
+
+O intuito desse projeto é monstar um servidor com as configurações minimas de websocket, que servirá de base para os proximos projetos que usem desta mesma tecnologia.
--- src/server.ts
+++ src/server.ts
@@ -3,6 +3,7 @@ import http from 'http';
 import { Server } from 'socket.io';
 
 const app = express();
+app.use(express.json());
 const serverHttp = http.createServer(app);
 const io = new Server(serverHttp, { cors: { origin: 'http://localhost:5173' } });
 
