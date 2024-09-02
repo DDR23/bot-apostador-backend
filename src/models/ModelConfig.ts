b6917c8f5adaae23fb409112fb75b0c471f75c45
omegatech-andre <omega.ti_ind@outlook.com>
2024-09-02 17:13:46 -0300

    feat:{mudanças de estrutura de codigo}

--- src/models/ModelConfig.ts
+++ src/models/ModelConfig.ts
@@ -1,34 +1,56 @@
-// src/models/Config.ts
-import { EstrategiesTenis, TypeConfigTenis } from "../types/TypeConfigTenis";
+import { TypeConfig } from "../types/TypeConfig";
 
-export class Config implements TypeConfigTenis {
+export class Config implements TypeConfig {
   _id: number;
   USER: string;
   PASSWORD: string;
   TIME_START: string;
   TIME_FINISH: string;
-  STOP_WIN: number;
-  STOP_LOSS: number;
-  ESTRATEGIES: EstrategiesTenis[];
   STATUS: boolean;
 
-  constructor(data: TypeConfigTenis) {
+  constructor(data: TypeConfig) {
     this._id = data._id;
     this.USER = data.USER;
     this.PASSWORD = data.PASSWORD;
     this.TIME_START = data.TIME_START;
     this.TIME_FINISH = data.TIME_FINISH;
-    this.STOP_WIN = data.STOP_WIN;
-    this.STOP_LOSS = data.STOP_LOSS;
-    this.ESTRATEGIES = data.ESTRATEGIES;
     this.STATUS = data.STATUS;
   }
-
   save(): void {
     console.log(`A configuração do usuario: ${this.USER} foi salva com sucesso.`);
     console.log('Config:', { ...this });
   }
 }
+
+
+
+
+
+
+
+
+
+// _id: number;
+// USER: string;
+// PASSWORD: string;
+// TIME_START: string;
+// TIME_FINISH: string;
+// STOP_WIN: number;
+// STOP_LOSS: number;
+// ESTRATEGIES: EstrategiesTenis[];
+// STATUS: boolean;
+
+// constructor(data: TypeConfigTenis) {
+//   this._id = data._id;
+//   this.USER = data.USER;
+//   this.PASSWORD = data.PASSWORD;
+//   this.TIME_START = data.TIME_START;
+//   this.TIME_FINISH = data.TIME_FINISH;
+//   this.STOP_WIN = data.STOP_WIN;
+//   this.STOP_LOSS = data.STOP_LOSS;
+//   this.ESTRATEGIES = data.ESTRATEGIES;
+//   this.STATUS = data.STATUS;
+// }
 
 // update(data: Partial<TypeConfigTenis>): void {
 //   Object.assign(this, data);
--- src/socket/Handlers/HandleConfig.ts
+++ src/socket/Handlers/HandleConfig.ts
@@ -1,9 +1,7 @@
 import { Socket } from "socket.io";
-import { TypeConfigTenis } from "../../types/TypeConfigTenis";
 import ControllerConfigPost from "../../controllers/config/ControllerConfigPost";
-import ControllerConfigGet from "../../controllers/config/ControllerConfigGet";
+import { TypeConfig } from "../../types/TypeConfig";
 
 export default function HandleConfig(socket: Socket) {
-  socket.on('config_post', (data: TypeConfigTenis) => ControllerConfigPost(socket, data));
-  socket.on('config_get', (user: TypeConfigTenis) => ControllerConfigGet(socket, user));
+  socket.on('CONFIG_POST', (data: TypeConfig) => ControllerConfigPost(socket, data));
 }
