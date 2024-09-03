01934ee5b1844ec0d16ef9a1f2ebf56922b249b1
omegatech-andre <omega.ti_ind@outlook.com>
2024-09-03 09:54:13 -0300

    feat:{adicionando controller de post config finalizado}

--- src/controllers/config/ControllerConfigPost.ts
+++ src/controllers/config/ControllerConfigPost.ts
@@ -4,16 +4,20 @@ import { TypeConfig } from '../../types/TypeConfig';
 
 export default async function ControllerConfigPost(socket: Socket, data: TypeConfig) {
   try {
-    if (!data || typeof data !== 'object') throw new Error('Dados inválidos.');
-    const config = await Config.save(data);
+    if (!data || typeof data !== 'object') {
+      socket.emit('CONFIG_POST_RES', {
+        title:'Erro',
+        message: 'Dados inválidos.'
+      });
+    };
+    await Config.save(data);
     socket.emit('CONFIG_POST_RES', {
       title: 'Sucesso',
       message: 'Configuração criada com sucesso!',
-      data: config,
     });
   } catch (error) {
     socket.emit('CONFIG_POST_RES', {
-      title: 'Erro inesperado',
+      title: 'Erro',
       message: (error as Error).message || 'A ação não pode ser finalizada.',
     });
   }
