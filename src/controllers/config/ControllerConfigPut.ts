ffd4af9badf1ab231a310c55cba808029ff558dd
DDR23 <roboreserva.andre@gmail.com>
2024-09-04 23:17:46 -0300

    fix:{agora controllerconfigput retorna um alert caso o front envie o data vazio}

--- src/controllers/config/ControllerConfigPut.ts
+++ src/controllers/config/ControllerConfigPut.ts
@@ -4,6 +4,14 @@ import { TypeConfig } from "../../types/TypeConfig";
 
 export default async function ControllerConfigPut(socket: Socket, id: string, data: Partial<TypeConfig>) {
   try {
+    console.log(data)
+    if (!data || typeof data !== 'object' || Object.keys(data).length === 0) {
+      socket.emit('CONFIG_PUT_RES', {
+        title: 'Alerta',
+        message: 'Nada foi alterado.'
+      });
+      return;
+    };
     const UpdatedConfig = await Config.update(id, data);
     socket.emit('CONFIG_PUT_RES', {
       title: 'Sucesso',
