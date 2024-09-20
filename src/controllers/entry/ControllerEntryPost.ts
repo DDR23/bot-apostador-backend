44ea00289166887230d33dd649d62ad228987c74
omegatech-andre <omega.ti_ind@outlook.com>
2024-09-20 10:08:03 -0300

    save

--- src/controllers/entry/ControllerEntryPost.ts
+++ src/controllers/entry/ControllerEntryPost.ts
@@ -6,30 +6,19 @@ import Strategy from '../../models/ModelStrategy';
 export default async function ControllerEntryPost(socket: Socket, data: TypeConfigToScraper) {
   try {
     const scraper = GetSocket();
-
     const strategies = await Strategy.findAllByConfig(data._id);
     data.CONFIG_STRATEGIES = strategies;
 
-    let eventCount = 0;
-    const handleScraperInitRes = (response: { title: string, message: string }) => {
-      eventCount++;
-      console.log(response);
-      const { title, message } = response;
-      socket.emit('ENTRY_POST_RES', {
-        title: title,
-        message: message,
-      });
-
-      if (eventCount >= 2) {
-        scraper.off('SCRAPER_INIT_RES', handleScraperInitRes);
-      }
-    };
-    scraper.on('SCRAPER_INIT_RES', handleScraperInitRes);
-
+    // const handleScraperInitRes = (response: { title: string, message: string }) => {
+    //   const { title, message } = response;
+    //   socket.emit('ENTRY_POST_RES', {
+    //     title: title,
+    //     message: message,
+    //   });
+    //   scraper.off('SCRAPER_INIT_RES', handleScraperInitRes);
+    // };
+    // scraper.on('SCRAPER_INIT_RES', handleScraperInitRes);
     scraper.emit('SCRAPER_INIT', data);
-
-    // Esperar o retorno do evento
-    // Aqui pode-se adicionar lógica adicional, se necessário
 
   } catch (error) {
     socket.emit('ENTRY_POST_RES', {
