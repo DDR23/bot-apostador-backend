89dec58af252864efbea22ace65541f2c72e0184
omegatech-andre <omega.ti_ind@outlook.com>
2024-09-23 11:55:06 -0300

    feat:{adicionando comunicação e notificação d eparada de bot}

--- src/controllers/scraper/ControllerScraperStop.ts
+++ src/controllers/scraper/ControllerScraperStop.ts
@@ -7,7 +7,18 @@ export default function ControllerScraperStop(io: Server, socket: Socket, data: 
     const scraper = GetSocket();
 
     // PARA O BOT
-    scraper.emit('SCRAPER_FINISH', data)
+    scraper.emit('SCRAPER_FINISH', data._id)
+
+    // OUVE REPOSTA DE PARADA
+    const handleScraperFinishRes = (response: { title: string, message: string }) => {
+      const { title, message } = response;
+      io.emit('SCRAPER_STOP_RES', {
+        title: title,
+        message: message
+      })
+      scraper.off('SCRAPER_FINISH_RES', handleScraperFinishRes)
+    }
+    scraper.on('SCRAPER_FINISH_RES', handleScraperFinishRes)
 
   } catch (error) {
     socket.emit('SCRAPER_STOP_RES', {
